8578830a80e3939aa5935a73a7ebf43a5dac8484
Eli <eliscolatour@gmail.com>
2018-04-16 18:00:53 +0100

    Substract the "join us" event form the default navigation handler

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Get a reference of DOM elements to use and store them in variables
 const sysMenu = document.querySelector('.sys-main__menu');
 sysMenu.addEventListener('click', function (e) {
-    e.preventDefault();
-    if( e.target != this ){
-        sectionID = e.target.getAttribute('href');
+    sectionID = e.target.getAttribute('href');
+    if( sectionID.charAt(0) === "#" ){
+        e.preventDefault();
         clickedSection = document.querySelector(sectionID);
         clickedSection.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
     }
